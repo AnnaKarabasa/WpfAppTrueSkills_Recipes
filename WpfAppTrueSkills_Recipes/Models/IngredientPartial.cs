5a103faf66b163177b98113417fa73aa76603f54
Gogs <gogs@fake.local>
2022-04-15 11:50:23 +0300

    List Ingredients (logic in code)

---
+++ WpfAppTrueSkills_Recipes/Models/IngredientPartial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppTrueSkills_Recipes.Models
+{
+    public partial class Ingredient
+    {
+        //public double Price
+        //{
+        //    get
+        //    {
+        //        return (double)Cost / CostForCount;
+        //    }
+        //}
+
+        public double Price => (double)Cost / CostForCount;
+    }
+}
--- WpfAppTrueSkills_Recipes/Pages/PageListIngredients.xaml.cs
+++ WpfAppTrueSkills_Recipes/Pages/PageListIngredients.xaml.cs
@@ -20,14 +20,28 @@ namespace WpfAppTrueSkills_Recipes.Pages
     /// </summary>
     public partial class PageListIngredients : Page
     {
+        Models.MyRecipesEntities _context = new Models.MyRecipesEntities();
+
         public PageListIngredients()
         {
             InitializeComponent();
+
+            RefreshData();
+        }
+
+        private void RefreshData()
+        {
+            List<Models.Ingredient> listIngredients = _context.Ingredients.ToList();
+            DGridIngredients.ItemsSource = listIngredients;
+
+            LblTotalQuantity.Content = listIngredients.Count + " наименований";
+            double sum = listIngredients.Sum(x => x.Price * x.AvailableCount);
+            LblTotalSum.Content = $"Запасов в холодильнике на сумму: {sum:N2} руб.";
         }
 
         private void BtnFirstPage_Click(object sender, RoutedEventArgs e)
         {
-
+            RefreshData();
         }
 
         private void BtnPreviousPage_Click(object sender, RoutedEventArgs e)
@@ -46,6 +60,16 @@ namespace WpfAppTrueSkills_Recipes.Pages
         }
 
         private void BorderPlus_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void LinkEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LinkDelete_Click(object sender, RoutedEventArgs e)
         {
 
         }
